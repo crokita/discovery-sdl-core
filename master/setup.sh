58569b70b8877e723b7baca95606099c2842202c
Christopher Rokita <chris@livio.io>
2016-07-20 13:44:42 -0400

    Contact etcd before running SDL core

--- master/setup.sh
+++ master/setup.sh
@@ -10,8 +10,8 @@ echo "Changing smartDeviceLink.ini HMI ServerAddress to ${IP}"
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
 perl -pi -e 's/127.0.0.1/'$IP'/g' /usr/sdl/src/appMain/smartDeviceLink.ini
 
-#Start SDL Core
-/usr/sdl/src/appMain/smartDeviceLinkCore
-
 #Post to the etcd database that this container exists
 curl -L -X PUT http://192.168.1.130:4001/v2/keys/servers/core -d value="${IP}"
+
+#Start SDL Core
+/usr/sdl/src/appMain/smartDeviceLinkCore
