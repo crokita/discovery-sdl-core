578c2501e673f3b79d413d907bfb7eda0ba1a19e
Christopher Rokita <chris@livio.io>
2016-07-20 14:32:52 -0400

    Add environment variable for host ip address

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-Hey. Use these commands to run me. You won't regret it
+Hey. Use these commands to run me. You won't regret it.
+Pass in the IP of the host to the containers that are being created as an environment variable
 
 master branch:
 
-```docker run -d -p 12435:12345 -p 8087:8087 crokita/sdl-core:master```
+```docker run -d -p 12435:12345 -p 8087:8087 -e "HOST_IP=127.0.0.1" crokita/sdl-core:master```
 
 develop branch:
 
--- master/setup.sh
+++ master/setup.sh
@@ -3,15 +3,13 @@
 # This script replaces the IP address in smartDeviceLink.ini with the machine's IP address and then runs SmartDeviceLink Core.
 # The IPis required to be replaced for the websocket connection with the HMI to function.
 
-# # Get the machine's IP address
-IP="$(ip addr show ${SDL_CORE_NETWORK_INTERFACE} | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-echo "Changing smartDeviceLink.ini HMI ServerAddress to ${IP}"
+echo "Changing smartDeviceLink.ini HMI ServerAddress to ${HOST_IP}"
 
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
-perl -pi -e 's/127.0.0.1/'$IP'/g' /usr/sdl/src/appMain/smartDeviceLink.ini
+perl -pi -e 's/127.0.0.1/'$HOST_IP'/g' /usr/sdl/src/appMain/smartDeviceLink.ini
 
 #Post to the etcd database that this container exists
-curl -L -X PUT http://192.168.1.130:4001/v2/keys/servers/core -d value="${IP}"
+curl -L -X PUT http://192.168.1.130:4001/v2/keys/servers/core -d value="${HOST_IP}"
 
 #Start SDL Core
 /usr/sdl/src/appMain/smartDeviceLinkCore
