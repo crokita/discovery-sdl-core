4f00e7170277cb6d9f3dd4278348f5e22b3a3981
Christopher Rokita <chris@livio.io>
2016-07-21 16:49:07 -0400

    Fix post to etcd

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Hey. Use these commands to run me. You won't regret it.
 
+Pass in the IP of the host to the containers that are being created as an environment variable
+
+
 master branch:
 
-```docker run -d -p 12435:12345 -p 8087:8087 -p 5050:5050 -p 5080:5080 -p 8090:8090 crokita/sdl-core:master```
+```docker run -d -p 12435:12345 -p 8087:8087 -p 5050:5050 -p 5080:5080 -p 8090:8090 -e "HOST_IP=127.0.0.1" crokita/sdl-core:master```
 
 develop branch:
 
-```docker run -d -p 12435:12345 -p 8087:8087 -p 5050:5050 -p 5080:5080 -p 8090:8090 crokita/sdl-core:develop```
+```docker run -d -p 12435:12345 -p 8087:8087 -p 5050:5050 -p 5080:5080 -p 8090:8090 -e "HOST_IP=127.0.0.1" crokita/sdl-core:develop```
--- develop/setup.sh
+++ develop/setup.sh
@@ -2,12 +2,13 @@
 
 # This script replaces the IP address in smartDeviceLink.ini with the machine's IP address and then runs SmartDeviceLink Core.
 # The IPis required to be replaced for the websocket connection with the HMI to function.
-HOST_IP="$(hostname -i)"
-echo "Changing smartDeviceLink.ini HMI ServerAddress to ${HOST_IP}"
+DOCKER_IP="$(hostname -i)"
+echo "Changing smartDeviceLink.ini HMI ServerAddress to ${DOCKER_IP}"
 
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
-perl -pi -e 's/127.0.0.1/'$HOST_IP'/g' /usr/sdl/src/appMain/smartDeviceLink.ini
+perl -pi -e 's/127.0.0.1/'$DOCKER_IP'/g' /usr/sdl/bin/smartDeviceLink.ini
 
+echo "Posting ${HOST_IP} to etcd"
 #Post to the etcd database that this container exists
 curl -L -X PUT http://192.168.1.130:4001/v2/keys/servers/core -d value="${HOST_IP}"
 
