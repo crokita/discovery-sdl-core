45caf52b8f525d6cc62436d186bad608ddc39a0f
Christopher Rokita <chris@livio.io>
2017-05-18 15:59:15 -0400

    Enable policy tables but allow all permissions for getting vehicle data

--- master/setup.sh
+++ master/setup.sh
@@ -9,8 +9,8 @@ DOCKER_IP="$(ip addr show ${CORE_NETWORK_INTERFACE} | grep "inet\b" | awk '{prin
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
 perl -pi -e 's/127.0.0.1/'$DOCKER_IP'/g' /usr/build/bin/smartDeviceLink.ini
 
-# Disable policy tables to allow full access to vehicle data and other things
-perl -pi -e 's/EnablePolicy = true/EnablePolicy = false/g' /usr/build/bin/smartDeviceLink.ini
+# Set the default preloaded policy table to allow access to all vehicle data
+perl -pi -e 's/\[\"Base-4\"\]/\[\"Base-4\", \"Location-1\", \"VehicleInfo-3\", \"Emergency-1\", \"DrivingChracteristics-3\"\]/g' /usr/build/bin/sdl_preloaded_pt.json
 
 # Add the ability to send logs to stdout
 perl -pi -e 's/ALL, SmartDeviceLinkCoreLogFile/ALL, SmartDeviceLinkCoreLogFile, Console/g' /usr/build/bin/log4cxx.properties
