d33c353b6afd2cd6accb697a37b19dae317532a0
Christopher Rokita <chris@livio.io>
2018-04-12 11:36:02 -0400

    Increase request timeout and remove writing logs to file

--- master/setup.sh
+++ master/setup.sh
@@ -9,10 +9,14 @@ DOCKER_IP="$(ip addr show ${CORE_NETWORK_INTERFACE} | grep "inet\b" | awk '{prin
 # Replace the IP address in smartDeviceLink.ini with the machines IP address
 perl -pi -e 's/127.0.0.1/'$DOCKER_IP'/g' /usr/build/bin/smartDeviceLink.ini
 
+# Increase the default request timeout from 10 seconds to 20 seconds
+perl -pi -e 's/Default.*/Default = 20000/g' /usr/build/bin/smartDeviceLink.ini
+
 # Set the default preloaded policy table to allow access to everything....... everything
 perl -pi -e 's/\[\"Base-4\"\]/\[\"Base-4\", \"Location-1\", \"Notifications\", \"DrivingCharacteristics-3\", \"VehicleInfo-3\", \"PropriataryData-1\", \"PropriataryData-2\", \"ProprietaryData-3\", \"Emergency-1\", \"Navigation-1\", \"Base-6\", \"OnKeyboardInputOnlyGroup\", \"OnTouchEventOnlyGroup\", \"DiagnosticMessageOnly\", \"DataConsent-2\", \"BaseBeforeDataConsent\", \"SendLocation\", \"WayPoints\", \"BackgroundAPT\", \"Notifications-RC\", \"RemoteControl\", \"HapticGroup\"\],\n\"moduleType\"\: \[\"CLIMATE\", \"RADIO\"\] /g' /usr/build/bin/sdl_preloaded_pt.json
-# Add the ability to send logs to stdout
-perl -pi -e 's/ALL, SmartDeviceLinkCoreLogFile/ALL, SmartDeviceLinkCoreLogFile, Console/g' /usr/build/bin/log4cxx.properties
+
+# Add the ability to send logs to stdout. Do not write logs to file
+perl -pi -e 's/ALL, SmartDeviceLinkCoreLogFile/ALL, Console/g' /usr/build/bin/log4cxx.properties
 
 #Start supervisord
 /usr/bin/supervisord
